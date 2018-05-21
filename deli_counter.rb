72f3eb4cbf326e91c929eb85dfce45984f8739b6
Elaine Anderson <eparie@bu.edu>
2018-05-21 19:48:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,13 @@
 require 'pry'
 
 def line(array)
-  customer_line = []
-if array.length > 0
-  array.each.with_index(1) do |name, index|
-   customer_line.push("#{index}. #{name}")
+  customer_line = String.new
+  if array.length > 0
+    array.each.with_index(1) do |name, index|
+      customer_line
+    end
+    puts "The line is currently: #{customer_line}"
+  else
+    puts "The line is currently empty."
   end
-  puts "The line is currently: #{customer_line}"
-end
 end
