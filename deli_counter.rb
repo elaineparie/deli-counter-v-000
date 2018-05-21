366b6382c3ecfe56ea79f2cfd75c0a239ecd075f
Elaine Anderson <eparie@bu.edu>
2018-05-21 20:02:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(array)
   customer_line = String.new
   if array.length > 0
     array.each.with_index(1) do |name, index|
-      customer_line << "#{index}. #{name}"
+      customer_line << "#{index}. #{name} "
     end
-    puts "The line is currently: #{customer_line} "
+    puts "The line is currently: #{customer_line}"
   else
     puts "The line is currently empty."
   end
