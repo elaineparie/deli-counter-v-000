8968a80d93ca3b354b3de946a851f2327b132939
Elaine Anderson <eparie@bu.edu>
2018-05-21 22:19:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,5 @@ end
 
 def take_a_number(array, name)
 array << "#{name}"
-  puts "Welcome, #{name}. You are number in line."
+  puts "Welcome, #{name}. You are number #{name.index} in line."
   end
